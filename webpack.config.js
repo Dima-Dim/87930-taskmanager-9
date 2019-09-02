04427e4d1660a59f38a1d1dca10f05b2ab31c672
Dima <dima@onmaster.ru>
2019-09-02 16:17:37 +0300

    Конфиг для style-loader и css-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,13 @@ module.exports = {
     port: 8080,
     compress: true,
     watchContentBase: true
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
