2005e717fc05e0b8835ffcfbc5ad5b3aa9184c51
Dima <dima@onmaster.ru>
2019-09-28 10:48:32 +0300

    Переписал Service Worker по принципу NetWork or Cache с использованием Timeout.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = `TASK_MANAGER`;
+const CACHE_NAME = `TASK_MANAGER_v2.7`;
+const TIMEOUT = 1000;
 
 self.addEventListener(`install`, (evt) => {
   console.log(`sw, install`, {evt});
@@ -6,10 +7,12 @@ self.addEventListener(`install`, (evt) => {
   evt.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        return cache.addAll([
-          `./`,
-          `./index.html`,
-          `./bundle.js`
+          return cache.addAll([
+            `./`,
+            `./index.html`,
+            `./css/normalize.css`,
+            `./css/style.css`,
+            `./bundle.js`,
           ]);
         }
       )
@@ -21,29 +24,59 @@ self.addEventListener(`activate`, (evt) => {
 });
 
 self.addEventListener(`fetch`, (evt) => {
-    console.log(`Запросили задачи`, {evt, request: evt.request});
+  console.log(`Запросили задачи`, {evt, request: evt.request});
 
-    evt.respondWith(
-      caches.match(evt.request)
-        .then((response) => {
-          console.log(`Find in cache`, {response});
-          if (response) {
-            return response
-          } else {
-            return fetch(evt.request)
-              .then(function (response) {
-                caches.open(CACHE_NAME)
-                  .then((cache) => cache.put(evt.request, response.clone()));
-                return response.clone();
-              });
-            }
-          })
-        .catch((err) => console.log({err}))
-    );
+  evt.respondWith(
+    fromNetwork(evt.request, TIMEOUT)
+      .then((response) => {
+        toCache(evt.request, response.clone());
+        return response;
+      })
+      .catch((err) => {
+        throw new Error(`From network error: ${err}`);
+        return fromCache(evt.request);
+      })
+  );
 
   console.log(`fetch`, {evt, request: evt.request});
 });
 
-self.addEventListener(`fetch`, (evt) => {
-});
+function fromCache(request, err) {
+  console.log(`fromCache`);
 
+  return caches.open(CACHE_NAME).then((cache) =>
+    cache.match(request).then((matching) =>
+      matching || Promise.reject('no-match')
+    ))
+    .catch((err) => new Error(`fetch error: ${err}`));
+}
+
+function toCache(request, response) {
+  console.log(`toCache`);
+
+  return caches.open(CACHE_NAME)
+    .then((cache) => cache.put(request, response))
+    .catch((err) => new Error(`Cache put error: ${err}`));
+}
+
+function update(request) {
+  console.log(`request`);
+
+  return caches.open(CACHE).then((cache) =>
+    fetch(request).then((response) =>
+      cache.put(request, response)
+    )
+  );
+}
+
+function fromNetwork(request, timeout) {
+  console.log(`fromNetwork`);
+
+  return new Promise((fulfill, reject) => {
+    let timeoutId = setTimeout(reject, timeout);
+    fetch(request).then((response) => {
+      clearTimeout(timeoutId);
+      fulfill(response);
+    }, reject);
+  });
+}
